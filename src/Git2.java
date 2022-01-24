79fe3c847d12d27ba35b26ed8f54dae290c0e64b
Ziaullah <94883774+ahmmad123@users.noreply.github.com>
2022-01-23 23:15:00 -0500

    Update Git2.java
    this is changes in repo

--- src/Git2.java
+++ src/Git2.java
@@ -3,7 +3,9 @@ public class Git2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+             
 	}
-
+public void methodA(){
+	sysytem.out.println("this is the changes from upstream, upstream means in remote repository");
+	
 }
